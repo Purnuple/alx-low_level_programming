793f3c5e1b89cff2940e546f302fc2e3a2f39a98
Purnuple <nndlovu426@gmail.com>
2022-09-11 13:43:56 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+
+		putchar(48 + i);
+		i++;
+
+	}
+	putchar('\n');
+	return (0);
+}
