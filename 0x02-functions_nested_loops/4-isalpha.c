b891a2e5891d1dd0134de8620a50afd7ac8748c2
Purnuple <nndlovu426@gmail.com>
2022-09-13 21:46:03 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - check if lowercase or uppercase
+ * @c: check if c is a letter
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	int i = _isalpha(c);
+
+	if (i > 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
