b52e4f94154d04144bac5813eab8d673db832b7e
Purnuple <nndlovu426@gmail.com>
2022-09-14 14:04:35 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - add two integers together
+ *@a: int num to be added
+ *@b: int num to be added
+ *Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
