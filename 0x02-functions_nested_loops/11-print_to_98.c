0003bea379ce5e32fb20a381ff71370eef54ef46
Purnuple <nndlovu426@gmail.com>
2022-09-14 14:27:52 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - check n and print 98 times
+ *
+ * @n: input
+ */
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+
+		for (count = n; count > 98; --count)
+		printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
+}
