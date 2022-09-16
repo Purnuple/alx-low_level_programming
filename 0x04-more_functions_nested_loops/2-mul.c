67c8fd058f0996ee20ca29a7d0a830c1b48e6106
Purnuple <nndlovu426@gmail.com>
2022-09-15 18:14:28 -0700

    Task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - check the code
+ * @a: is integer to take number
+ * @b: is integer to take number
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
