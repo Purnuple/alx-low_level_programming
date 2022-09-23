8bdd8a759bcd275f645f58eb8316b04a9f4d956f
Purnuple <nndlovu426@gmail.com>
2022-09-22 17:36:36 -0700

    Task 10

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *infinite_add - adds two numbers
+ * @n1 : char used
+ * @n2 : char used
+ * @r : char used
+ * @size_r : int used
+ * Return: 1 or 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
