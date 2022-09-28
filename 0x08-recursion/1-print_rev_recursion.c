e9b8ff76434019aa43289968397b00608ab8c549
Purnuple <nndlovu426@gmail.com>
2022-09-28 14:06:10 -0700

    Task 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * _print_rev_recursion - prints str in reverse
+ * @s: pointer
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+		if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
