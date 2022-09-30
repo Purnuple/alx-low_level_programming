152e38ef77734e25c9446838bbfd65f5a90ac412
Purnuple <nndlovu426@gmail.com>
2022-09-30 04:57:13 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name
+ * @argc : integer used
+ * @argv : char used
+ * Return: 0 Always
+ */
+int main(int argc, char **argv)
+
+{	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
