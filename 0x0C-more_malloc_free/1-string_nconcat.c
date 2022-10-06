7f4e596190a85bbfb8eb1371440a6df46e8a2dea
Purnuple <nndlovu426@gmail.com>
2022-10-06 04:08:49 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,4 +20,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
+	for (i = 0; s1[i]; i++)
+		len++;
 
+	mem = malloc(sizeof(char) * (len + 1));
+
+	if (mem == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		mem[len++] = s1[i];
+
+	for (i = 0; s2[i] && i < n; i++)
+		mem[len++] = s2[i];
+
+	mem[len] = '\0';
+
+	return (mem);
+}
