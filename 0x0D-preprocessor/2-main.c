44d7b95da0a65437addb9e098e4a3fa34eb2a52a
Purnuple <nndlovu426@gmail.com>
2022-10-10 12:04:20 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define FILE_NAME__FILE__
+
+/**
+ * main -prints the name of the file
+ *
+ * Return:Always success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
